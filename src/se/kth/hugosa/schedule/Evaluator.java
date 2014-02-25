b9b1fe72711a6f00f6cb66f837e723c8644cd247
Hugo Sandelius <hugo.sandelius@gmail.com>
2014-02-25 20:39:12 +0100

    Started coding an evaluation function

--- src/se/kth/hugosa/schedule/Evaluator.java
+++ src/se/kth/hugosa/schedule/Evaluator.java
@@ -1,9 +1,22 @@
 package se.kth.hugosa.schedule;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Evaluator {
-    public int evaluateSchedule(List<Schedule> schedule) {
+    //needs to also take constraints in the future
+    public int evaluateSchedule(ArrayList<Schedule> schedules) {
+        for (int i = 0; i < schedules.get(0).days.size(); i++) {
+           for (Schedule schedule : schedules) {
+               Day d = schedule.days.get(i);
+
+           }
+        }
         return 0;
+    }
+
+    //checks for collisions between a list of classes held at the same time
+    private boolean collides(List<SchoolClass> classes) {
+        return false;
     }
 }
