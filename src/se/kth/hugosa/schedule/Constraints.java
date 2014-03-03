ee13e100156c03c6095ca6980b17276683835313
hugo <hugosa@kth.se>
2014-03-03 14:35:26 +0100

    Fleshed out Constraints

--- src/se/kth/hugosa/schedule/Constraints.java
+++ src/se/kth/hugosa/schedule/Constraints.java
@@ -1,5 +1,16 @@
 package se.kth.hugosa.schedule;
 
 
+import java.util.List;
+
 public class Constraints {
+    private List<ScheduleElement> elements;
+
+    public Constraints(List<ScheduleElement> scheduleElement) {
+
+    }
+
+    public List<ScheduleElement> getScheduleElements() {
+        return elements;
+    }
 }
