03f086fe72ef16562f6c9ab9775bc5dec5d5b8c9
Simon Forssell <simonfacoin@gmail.com>
2014-03-06 14:16:15 +0100

    Set up genetic operator

--- src/se/kth/hugosa/schedule/Constraints.java
+++ src/se/kth/hugosa/schedule/Constraints.java
@@ -1,5 +1,30 @@
 package se.kth.hugosa.schedule;
 
 
+import java.util.List;
+
 public class Constraints {
+    private List<ScheduleElement> elements;
+    private int scheduleWeeks;
+    private int classrooms;
+
+    public Constraints(List<ScheduleElement> scheduleElements) {
+        this.elements = scheduleElements;
+    }
+
+    public List<ScheduleElement> getScheduleElements() {
+        return elements;
+    }
+
+    public int getScheduleWeeks() {
+        return scheduleWeeks;
+    }
+    
+    public int getNumSlots(){
+    	return classrooms * 4 * scheduleWeeks * 5; //5 days per week, 4 slots per classroom and day
+    }
+    
+    public int getNumElements(){
+    	return elements.size();
+    }
 }
--- src/se/kth/hugosa/schedule/ScheduleFitnessFunction.java
+++ src/se/kth/hugosa/schedule/ScheduleFitnessFunction.java
@@ -5,22 +5,16 @@ import org.jgap.IChromosome;
 import java.util.ArrayList;
 
 public class ScheduleFitnessFunction extends FitnessFunction {
-	Evaluator e;
-	Constraints constraints;
-	ArrayList<ScheduleElement> elements;
+	private Evaluator e;
+	private Constraints constraints;
 	
-	public ScheduleFitnessFunction( Constraints constraints, ArrayList<ScheduleElement> list){
+	public ScheduleFitnessFunction( Constraints constraints){
 		this.constraints = constraints;
-		elements = list;
 	}
 	
 	@Override
     public double evaluate( IChromosome c ){
-        ArrayList<Schedule> s = getSchedules(c);
+        ArrayList<Schedule> s = GeneticSchedule.generateSchedule(c);
         return (double) e.evaluateSchedule(s, constraints);
     }
-	
-	private static ArrayList<Schedule> getSchedules( IChromosome c ){
-		return null;
-	}
 }
