c26cf0a55e2f64f27f7517ff1f74c0fc7663f3cf
hugo <hugosa@kth.se>
2014-03-06 14:43:18 +0100

    Added constructor to TimeSlot

--- src/se/kth/hugosa/schedule/TimeSlot.java
+++ src/se/kth/hugosa/schedule/TimeSlot.java
@@ -5,4 +5,9 @@ import java.util.List;
 public class TimeSlot {
     public ScheduleElement scheduleElement;
     public Classroom classroom;
+
+    public TimeSlot(Classroom classroom, ScheduleElement scheduleElement) {
+        this.classroom = classroom;
+        this.scheduleElement = scheduleElement;
+    }
 }
