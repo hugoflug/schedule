89e9c2a786055d4f6316a5c9df2cc409328d4447
Simon Forssell <simonfacoin@gmail.com>
2014-04-14 00:00:49 +0200

    Changed the iteration graphs to separate plots

--- results/matlab/plotSmallResults.m
+++ results/matlab/plotSmallResults.m
@@ -4,13 +4,13 @@ geneticSmallResults;
 
 fontsize1 = 12;
 fontsize2 = 15;
-plot([1:1:size(tabu_avg,2)],tabu_avg,'b',[1:1:size(genetic_avg,2)],genetic_avg,'g');
-%subplot(2,1,1); plot(Tabu(:,1),Tabu(:,2));
-%title('Tabu search','fontsize',fontsize2)
+%plot([1:1:size(tabu_avg,2)],tabu_avg,'b',[1:1:size(genetic_avg,2)],genetic_avg,'g');
+subplot(2,1,1); plot(tabu_avg);
+title('Tabu search','fontsize',fontsize2)
 xlabel('Iterations','fontsize',fontsize2);
 ylabel('Evaluation score','fontsize',fontsize2);
-legend('Tabu search', 'Genetic algorithm');
-%subplot(2,1,2); plot(Genetic(:,1),Genetic(:,2))
-%title('Genetic algorithm','fontsize',fontsize2)
-%xlabel('Iterations','fontsize',fontsize1);
-%ylabel('Evaluation score','fontsize',fontsize1);
+%legend('Tabu search', 'Genetic algorithm');
+subplot(2,1,2); plot(genetic_avg)
+title('Genetic algorithm','fontsize',fontsize2)
+xlabel('Iterations','fontsize',fontsize1);
+ylabel('Evaluation score','fontsize',fontsize1);
