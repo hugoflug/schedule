ec81fd3d989702ecc5443bf67c8c24d8e2abb4b3
Simon Forssell <simonfacoin@gmail.com>
2014-04-13 23:17:49 +0200

    Added some figures

--- results/matlab/plotMediumResults.m
+++ results/matlab/plotMediumResults.m
@@ -1,10 +1,10 @@
 clear;
-tabuMediumTestResults;
-geneticMediumTestResults;
+tabuLargeResults;
+geneticLargeResults;
 
 fontsize1 = 12;
 fontsize2 = 15;
-plot(Tabu(1:1097,1),Tabu(1:1097,2),'b',Genetic(1:2000,1),Genetic(1:2000,2),'g');
+plot([1:1:size(tabu_avg,2)],tabu_avg,'b',[1:1:size(genetic_avg,2)],genetic_avg,'g');
 %subplot(2,1,1); plot(Tabu(:,1),Tabu(:,2));
 %title('Tabu search','fontsize',fontsize2)
 xlabel('Iterations','fontsize',fontsize2);
